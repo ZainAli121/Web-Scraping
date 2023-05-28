4ed04231c9f663c729f4ce84dc3f9973daca6a2f
Zain <learnerstudent001@gmail.com>
2023-05-28 10:11:19 +0500

    Updated

--- web_scraping/ecom.py
+++ web_scraping/ecom.py
@@ -1,12 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.levi.com/US/en_US/search/T-shirts"
-# headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+url = "https://breakout.com.pk/collections/men-shirts"
 
 r = requests.get(url)
-soup = BeautifulSoup(r.content, "html.parser")
-print(soup.prettify())
+soup = BeautifulSoup(r, 'html.parser')
 
-
-# In Progress.....................
+shirts= soup.find_all('li', class_="product")
+print(shirts)
